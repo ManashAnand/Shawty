8d2e301f5d532940582a7b565cc6e23032d204a2
unknown <anandmanash321@gmail.com>
2024-08-11 14:33:12 +0530

    added a link card

--- actions/apiUrls.ts
+++ actions/apiUrls.ts
@@ -12,9 +12,17 @@ export async function getUrls(user_id: string) {
     .eq("user_id", user_id);
 
   if (error) {
-    console.error(error.message) 
+    console.error(error.message);
     throw new Error("Unable to load URLs");
   }
   return data;
 }
 
+export async function deleteUrl(id: string) {
+  const { data, error } = await supabase.from("urls").delete().eq("id", id);
+  if (error) {
+    console.error(error.message);
+    throw new Error("Unable to Delete URLs");
+  }
+  return data;
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,25 +1,20 @@
 "use client";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import React from "react";
-import { BarLoader, ClockLoader } from "react-spinners";
-import { useEffect, useState } from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import React, { useState } from "react";
+import { ClockLoader } from "react-spinners";
 import { Filter } from "lucide-react";
 import { Input } from "@/components/ui/input";
-import { useQuery } from "@tanstack/react-query";
-import { getUrls } from "@/actions/apiUrls";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { deleteUrl, getUrls } from "@/actions/apiUrls";
 import { useAuthenticateState } from "@/actions/zustand";
 import { useRouter } from "next/navigation";
 import { getClicksForUrl } from "@/actions/apiClicks";
+import LinkCard from "@/components/custom/LinkCard";
 
 const Dashboard = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const router = useRouter();
+  const queryClient = useQueryClient();
 
   const isAuth = useAuthenticateState((state) => state.isAuthenticatad);
   if (!isAuth) router.push("/auth");
@@ -27,13 +22,16 @@ const Dashboard = () => {
   const user = useAuthenticateState((state) => state.user);
   const userId = user?.user?.id;
 
-  const { isPending, error, data: urls } = useQuery({
+  const {
+    isPending,
+    error,
+    data: urls,
+  } = useQuery({
     queryKey: ["repoData", userId],
     queryFn: () => getUrls(userId),
   });
 
   const url_id = urls?.map((u) => u.id.toString()) || [];
-  console.log(url_id)
   const {
     data: clicks,
     error: clicksError,
@@ -44,17 +42,29 @@ const Dashboard = () => {
     enabled: !!url_id.length,
   });
 
-  if (isPending || clicksPending) return <ClockLoader />;
+  const { mutate: deleteFn, isPending: isDeleting } = useMutation({
+    mutationFn: (id: string) => deleteUrl(id), // Pass the id here
+    onSuccess: () => {
+      // @ts-ignore
+      queryClient.invalidateQueries(["repoData", userId]);
+    },
+  });
+
+  if (isPending || clicksPending)
+    return (
+      <div className="w-full flex justify-between items-center">
+        <ClockLoader />;
+      </div>
+    );
   if (error || clicksError) return "Error";
 
   const filteredUrls = urls?.filter((url) =>
     url.title.toLowerCase().includes(searchQuery.toLowerCase())
   );
-  console.log(urls)
-  console.log(clicks)
+
   return (
     <>
-      <div className="flex flex-col gap-8">-
+      <div className="flex flex-col gap-8">
         <div className="grid grid-cols-2 gap-4">
           <Card>
             <CardHeader>
@@ -75,7 +85,6 @@ const Dashboard = () => {
         </div>
         <div className="flex justify-between">
           <h1 className="text-4xl font-extrabold">My Links</h1>
-          {/* <CreateLink /> */}
         </div>
         <div className="relative">
           <Input
@@ -86,9 +95,15 @@ const Dashboard = () => {
           />
           <Filter className="absolute top-2 right-2 p-1" />
         </div>
-        {(filteredUrls || [])?.map((url, i) => {
-          return <div key={i}>{url?.custom_url}</div>;
-        })}
+        {(filteredUrls || []).map((url, i) => (
+          <LinkCard
+            key={i}
+            url={url}
+            userId={userId}
+            deleteFn={deleteFn}
+            isDeleting={isDeleting}
+          />
+        ))}
       </div>
     </>
   );
